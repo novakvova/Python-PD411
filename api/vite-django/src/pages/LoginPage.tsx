1560bfce01c243635cefc44fed3ff3b117783dbf
novakvova <novakvova@gmail.com>
2026-07-01 17:50:14 +0300

    Workign is good template input form

--- api/vite-django/src/pages/LoginPage.tsx
+++ api/vite-django/src/pages/LoginPage.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router";
 import * as z from "zod";
 import {Controller, useForm} from "react-hook-form";
 import {zodResolver} from "@hookform/resolvers/zod";
+import {FormInput} from "../components/FormInput.tsx";
 
 const EyeIcon = ({ open }: { open: boolean }) => open ? (
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"
@@ -108,38 +109,11 @@ const LoginPage = () => {
 
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
 
-                        {/* Email */}
-                        <Controller
-                            name="email"
+                        <FormInput
                             control={form.control}
-                            render={({ field, fieldState }) => (
-                                <div>
-                                    <label className="block text-sm font-medium text-slate-700 dark:text-slate-300 mb-1.5">
-                                        Електронна пошта
-                                    </label>
-                                    <input
-                                        type="text"
-                                        {...field}
-                                        placeholder="kozak@sich.ua"
-                                        className={`
-                    w-full px-4 py-2.5 rounded-xl text-sm
-                    bg-slate-50 dark:bg-slate-800
-                    border ${fieldState.error ? "border-red-400 dark:border-red-500" : "border-slate-200 dark:border-slate-700"}
-                    text-slate-900 dark:text-slate-100
-                    placeholder:text-slate-400 dark:placeholder:text-slate-500
-                    outline-none
-                    focus:border-indigo-400 dark:focus:border-indigo-500
-                    focus:ring-2 focus:ring-indigo-400/20 dark:focus:ring-indigo-500/20
-                    transition-all duration-200
-                `}
-                                    />
-                                    {fieldState.error && (
-                                        <p className="mt-1.5 text-sm text-red-500 dark:text-red-400">
-                                            {fieldState.error.message}
-                                        </p>
-                                    )}
-                                </div>
-                            )}
+                            name="email"
+                            label="Електронна пошта"
+                            placeholder="kozak@sich.ua"
                         />
 
                         {/* Password */}
